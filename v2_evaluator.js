3bf6a1dc61b95eb89d682dfd6a47c15bf68c2cbb
Hoang-Nam Chu <74799165+nam-chu@users.noreply.github.com>
2025-03-19 11:09:10 -0400

    Update v2_evaluator.js

--- v2_evaluator.js
+++ v2_evaluator.js
@@ -25,6 +25,7 @@ const flightParameter = new Map([
     ]
 ]);
 
+/*
 // Public Transport
 const ptParameter = new Map([
     ["train", 0.08],
@@ -34,7 +35,7 @@ const ptParameter = new Map([
 
 
 // Car
-/*
+
 const fuelParameter = new Map([
     ["petrol", 1.53],
     ["diesel", 1.54],
@@ -157,36 +158,29 @@ function buildEvaluatorSettings(surveySettings) {
             diet: surveySettings.diet,
             selectDiet: surveySettings.diet
         },
-        reduceKilometrageCar: {
-            selected: false,
-            enabled: true,
-            visible: surveySettings.hasAccessToCar,
-            carKilometrageYearly: surveySettings.carKilometrageYearly,
-            select: 1.0
-        },
         shortFlights: {
             selected: false,
             enabled: true,
             visible: surveySettings.numShortFlights > 0,
             numShortFlights: surveySettings.numShortFlights,
-            select: 1
-        },
+            select: 1}
+        // },
 
-        mediumFlights: {
-            selected: false,
-            enabled: true,
-            visible: surveySettings.numMediumFlights > 0,
-            numMediumFlights: surveySettings.numMediumFlights,
-            select: 1
-        },
+        // mediumFlights: {
+        //     selected: false,
+        //     enabled: true,
+        //     visible: surveySettings.numMediumFlights > 0,
+        //     numMediumFlights: surveySettings.numMediumFlights,
+        //     select: 1
+        // },
 
-        longFlights: {
-            selected: false,
-            enabled: true,
-            visible: surveySettings.numLongFlights > 0,
-            numLongFlights: surveySettings.numLongFlights,
-            select: 1
-        }
+        // longFlights: {
+        //     selected: false,
+        //     enabled: true,
+        //     visible: surveySettings.numLongFlights > 0,
+        //     numLongFlights: surveySettings.numLongFlights,
+        //     select: 1
+        // }
     };
 
     evaluatorSettings.initialDiet = calculateDiet(evaluatorSettings);
@@ -217,85 +211,85 @@ function calculateMobility(mobilitySettings) {
     var mobility = 0;
 
     // car
-    if (mobilitySettings.replaceCar.car != "") { // only do all this if there is a access to a car
-        var existingCar = mobilitySettings.replaceCar.car;
-        var carKilometrage = mobilitySettings.reduceKilometrageCar.carKilometrageYearly;
-        var variableCostsExistingCar = carKilometrage * carParameter.get(existingCar).get("fuel-consumption") / 100 * carParameter.get(existingCar).get("fuel-price") +
-            carKilometrage * carParameter.get(existingCar).get("electricity-consumption") / 100 * carParameter.get(existingCar).get("electricity-price");
-        var fixedCostsExistingCar = 2650 + 0.134 * mobilitySettings.sellCar.carValue;
+    // if (mobilitySettings.replaceCar.car != "") { // only do all this if there is a access to a car
+    //     var existingCar = mobilitySettings.replaceCar.car;
+    //     var carKilometrage = mobilitySettings.reduceKilometrageCar.carKilometrageYearly;
+    //     var variableCostsExistingCar = carKilometrage * carParameter.get(existingCar).get("fuel-consumption") / 100 * carParameter.get(existingCar).get("fuel-price") +
+    //         carKilometrage * carParameter.get(existingCar).get("electricity-consumption") / 100 * carParameter.get(existingCar).get("electricity-price");
+    //     var fixedCostsExistingCar = 2650 + 0.134 * mobilitySettings.sellCar.carValue;
 
-        var actualCar = existingCar;
-        var variableCostsActualCar = variableCostsExistingCar;
-        var fixedCostsActualCar = fixedCostsExistingCar;
-        var actualCarKilometrage = carKilometrage;
+    //     var actualCar = existingCar;
+    //     var variableCostsActualCar = variableCostsExistingCar;
+    //     var fixedCostsActualCar = fixedCostsExistingCar;
+    //     var actualCarKilometrage = carKilometrage;
 
-        if (!mobilitySettings.sellCar.selected) {
-            mobilitySettings.sellCar.investment = "";
-            mobilitySettings.sellCar.annual = "";
+    //     if (!mobilitySettings.sellCar.selected) {
+    //         mobilitySettings.sellCar.investment = "";
+    //         mobilitySettings.sellCar.annual = "";
 
-            if (mobilitySettings.replaceCar.selected) {
-                actualCar = mobilitySettings.replaceCar.selectCar;
-                variableCostsActualCar = carKilometrage * carParameter.get(actualCar).get("fuel-consumption") / 100 * carParameter.get(actualCar).get("fuel-price") +
-                        carKilometrage * carParameter.get(actualCar).get("electricity-consumption") / 100 * carParameter.get(actualCar).get("electricity-price");
-                var priceActualCar = carParameter.get(actualCar).get("price");
-                fixedCostsActualCar = 2650 + 0.134 * priceActualCar;
+    //         if (mobilitySettings.replaceCar.selected) {
+    //             actualCar = mobilitySettings.replaceCar.selectCar;
+    //             variableCostsActualCar = carKilometrage * carParameter.get(actualCar).get("fuel-consumption") / 100 * carParameter.get(actualCar).get("fuel-price") +
+    //                     carKilometrage * carParameter.get(actualCar).get("electricity-consumption") / 100 * carParameter.get(actualCar).get("electricity-price");
+    //             var priceActualCar = carParameter.get(actualCar).get("price");
+    //             fixedCostsActualCar = 2650 + 0.134 * priceActualCar;
 
-                mobilitySettings.replaceCar.investment = priceActualCar - mobilitySettings.sellCar.carValue;
-                mobilitySettings.replaceCar.annual = (fixedCostsActualCar - fixedCostsExistingCar) + (variableCostsActualCar - variableCostsExistingCar);
+    //             mobilitySettings.replaceCar.investment = priceActualCar - mobilitySettings.sellCar.carValue;
+    //             mobilitySettings.replaceCar.annual = (fixedCostsActualCar - fixedCostsExistingCar) + (variableCostsActualCar - variableCostsExistingCar);
 
-                mobilitySettings.sum.investment += mobilitySettings.replaceCar.investment;
-                mobilitySettings.sum.annual += mobilitySettings.replaceCar.annual;
-            } else {
-                mobilitySettings.replaceCar.investment = "";
-                mobilitySettings.replaceCar.annual = "";
-            }
+    //             mobilitySettings.sum.investment += mobilitySettings.replaceCar.investment;
+    //             mobilitySettings.sum.annual += mobilitySettings.replaceCar.annual;
+    //         } else {
+    //             mobilitySettings.replaceCar.investment = "";
+    //             mobilitySettings.replaceCar.annual = "";
+    //         }
 
-            if (mobilitySettings.reduceKilometrageCar.selected) {
-                actualCarKilometrage = mobilitySettings.reduceKilometrageCar.carKilometrageYearly * (1 - mobilitySettings.reduceKilometrageCar.select);
+    //         if (mobilitySettings.reduceKilometrageCar.selected) {
+    //             actualCarKilometrage = mobilitySettings.reduceKilometrageCar.carKilometrageYearly * (1 - mobilitySettings.reduceKilometrageCar.select);
 
-               mobilitySettings.reduceKilometrageCar.investment = 0;
-               mobilitySettings.reduceKilometrageCar.annual = - variableCostsActualCar * (1 - actualCarKilometrage / carKilometrage);
+    //            mobilitySettings.reduceKilometrageCar.investment = 0;
+    //            mobilitySettings.reduceKilometrageCar.annual = - variableCostsActualCar * (1 - actualCarKilometrage / carKilometrage);
 
-               mobilitySettings.sum.investment += mobilitySettings.reduceKilometrageCar.investment;
-               mobilitySettings.sum.annual += mobilitySettings.reduceKilometrageCar.annual;
-            } else {
-                mobilitySettings.reduceKilometrageCar.investment = "";
-                mobilitySettings.reduceKilometrageCar.annual = "";
-            }
+    //            mobilitySettings.sum.investment += mobilitySettings.reduceKilometrageCar.investment;
+    //            mobilitySettings.sum.annual += mobilitySettings.reduceKilometrageCar.annual;
+    //         } else {
+    //             mobilitySettings.reduceKilometrageCar.investment = "";
+    //             mobilitySettings.reduceKilometrageCar.annual = "";
+    //         }
 
-            var carCo2 = carParameter.get(actualCar).get("co2");
-            var carValue = carCo2 *actualCarKilometrage / 1_000_000;
-            mobility += carValue;
-        } else { // i.e. we sell the car
-            mobilitySettings.sellCar.investment = - mobilitySettings.sellCar.carValue;
-            mobilitySettings.sellCar.annual = - fixedCostsActualCar - variableCostsExistingCar;
+    //         var carCo2 = carParameter.get(actualCar).get("co2");
+    //         var carValue = carCo2 *actualCarKilometrage / 1_000_000;
+    //         mobility += carValue;
+    //     } else { // i.e. we sell the car
+    //         mobilitySettings.sellCar.investment = - mobilitySettings.sellCar.carValue;
+    //         mobilitySettings.sellCar.annual = - fixedCostsActualCar - variableCostsExistingCar;
 
-            mobilitySettings.sum.investment += mobilitySettings.sellCar.investment;
-            mobilitySettings.sum.annual += mobilitySettings.sellCar.annual;
+    //         mobilitySettings.sum.investment += mobilitySettings.sellCar.investment;
+    //         mobilitySettings.sum.annual += mobilitySettings.sellCar.annual;
 
-            // if we sell the car, we automatically reduce the kilometrage by 100% - all savings are taken into account in the ""sell car"" option
-            mobilitySettings.reduceKilometrageCar.investment = "";
-            mobilitySettings.reduceKilometrageCar.annual = "";
-            mobilitySettings.replaceCar.investment = "";
-            mobilitySettings.replaceCar.annual = "";
-        }
-    }
+    //         // if we sell the car, we automatically reduce the kilometrage by 100% - all savings are taken into account in the ""sell car"" option
+    //         mobilitySettings.reduceKilometrageCar.investment = "";
+    //         mobilitySettings.reduceKilometrageCar.annual = "";
+    //         mobilitySettings.replaceCar.investment = "";
+    //         mobilitySettings.replaceCar.annual = "";
+    //     }
+    // }
     // pt
-    ptValue = 0;
-    ptValue += ptParameter.get("train") * mobilitySettings.trainKilometrageWeekly * 52 / 1000;
-    ptValue += ptParameter.get("tram") * mobilitySettings.tramKilometrageWeekly * 52 / 1000;
-    ptValue += ptParameter.get("bus") * mobilitySettings.busKilometrageWeekly * 52 / 1000;
-    mobility += ptValue;
+    // ptValue = 0;
+    // ptValue += ptParameter.get("train") * mobilitySettings.trainKilometrageWeekly * 52 / 1000;
+    // ptValue += ptParameter.get("tram") * mobilitySettings.tramKilometrageWeekly * 52 / 1000;
+    // ptValue += ptParameter.get("bus") * mobilitySettings.busKilometrageWeekly * 52 / 1000;
+    // mobility += ptValue;
 
     // flights
     flightsValue = 0;
     numShortFlights = mobilitySettings.shortFlights.numShortFlights;
-    numMediumFlights = mobilitySettings.mediumFlights.numMediumFlights;
-    numLongFlights = mobilitySettings.longFlights.numLongFlights;
+    // numMediumFlights = mobilitySettings.mediumFlights.numMediumFlights;
+    // numLongFlights = mobilitySettings.longFlights.numLongFlights;
    
     flightsValue += numShortFlights * flightParameter.get("short").get("co2");
-    flightsValue += numMediumFlights * flightParameter.get("medium").get("co2");
-    flightsValue += numLongFlights * flightParameter.get("long").get("co2");
+    // flightsValue += numMediumFlights * flightParameter.get("medium").get("co2");
+    // flightsValue += numLongFlights * flightParameter.get("long").get("co2");
     mobility += flightsValue;
 
     return mobility;
