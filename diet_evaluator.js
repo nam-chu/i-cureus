c23a876602f69a22ec52685f70600652f4a2ed00
Hoang-Nam Chu <74799165+nam-chu@users.noreply.github.com>
2025-03-19 16:19:48 -0400

    Add files via upload

---
+++ diet_evaluator.js
@@ -0,0 +1,59 @@
+
+// Start by declaring relevant data structures
+
+// Diet
+const dietParameter = new Map([
+    ["omnivore", 1.837],
+    ["flexitarian", 1.495],
+    ["vegetarian", 1.380],
+    ["vegan", 1.124]
+]);
+
+function getStartingTotal(surveySettings) {
+    var evaluatorSettings = buildEvaluatorSettings(surveySettings);
+    return calculateActualValues(evaluatorSettings);
+}
+
+function buildEvaluatorSettings(surveySettings) {
+    evaluatorSettings = {
+        // global settings
+        enableDebug: surveySettings.enableDebug,
+        language: surveySettings.language,
+        locale: surveySettings.locale,
+        hiddenTextSelector: surveySettings.hiddenTextSelector,
+
+        diet: {
+            selected: false,
+            enabled: true,
+            visible: true,
+            diet: surveySettings.diet,
+            selectDiet: surveySettings.diet
+        }
+    };
+
+    evaluatorSettings.initialDiet = calculateDiet(evaluatorSettings);
+    return evaluatorSettings;
+}
+
+function calculateActualValues(settings) {
+
+    var actualDiet = calculateDiet(settings);
+    var actual = {
+        actualDiet,
+        actualTotal
+    }
+    return actual;
+}
+
+function calculateDiet(dietSettings) {
+    if (dietSettings.diet.selected) {
+        var value = dietParameter.get(dietSettings.diet.selectDiet);
+        if (value == null) {
+            console.log("Unknown diet option selected: " + dietSettings.diet);
+            return 0;
+        } else return value;
+    } else {
+        return dietParameter.get(dietSettings.diet.diet);
+    }
+}
+
