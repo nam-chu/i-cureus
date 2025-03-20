581439e25657376fe2354303fadcd8fb2064d171
Hoang-Nam Chu <74799165+nam-chu@users.noreply.github.com>
2025-03-19 22:46:30 -0400

    Add files via upload

---
+++ flights_evaluator-settings.js
@@ -0,0 +1,50 @@
+function getDiet(val) {
+    // 1-based index (1,2,3,4)
+    let diet = ['', 'omnivore', 'flexitarian', 'vegetarian', 'vegan'];
+    let choice = extractSelectedChoices(val);
+    return diet[choice];
+}
+
+function getNbFlights(valNbFlightsTotal, valNbFlights) {
+    let totalChoice = extractSelectedChoices(valNbFlightsTotal); // 0 if no, 1 if yes
+    return (totalChoice != 0) ? parseInt(valNbFlights) : 0;
+}
+
+function extractSelectedChoices(answer, forceReturnArray = false) {
+    let NO_ANSWER = 0;
+    var indices = [];
+    if (answer == "") {
+        indices.push(NO_ANSWER);
+    } else {
+        while(answer.includes(",")) {
+            // read number
+            indices.push(parseInt(answer.charAt(0)));
+            // delete the read number and ", "
+            answer = answer.substring(3);
+        }
+        // last one
+        indices.push(parseInt(answer.charAt(0)));
+    }
+
+    if (indices.length == 1 && !forceReturnArray) {
+        return indices[0];
+    } else {
+        return indices;
+    }
+}
+
+function parseIntOrZero(answer) {
+    let parsed = parseInt(answer);
+    return isNaN(parsed) ? 0 : parsed;
+}
+
+function formatSurveySettings(data) {
+
+    var surveySettings = {
+        diet: getDiet(data.q1),
+        numShortFlights: getNbFlights(data.q2, data.q3)
+    };
+
+    return surveySettings;
+}
+
---
+++ flights_evaluator.js
@@ -0,0 +1,97 @@
+
+// Start by declaring relevant data structures
+
+// Diet
+const dietParameter = new Map([
+    ["omnivore", 1.837],
+    ["flexitarian", 1.495],
+    ["vegetarian", 1.380],
+    ["vegan", 1.124]
+]);
+
+const flightParameter = new Map([
+    ["short", new Map([
+        ["co2", 0.5]
+        ])
+    ],
+    ["medium", new Map([
+        ["co2", 2]
+        ])
+    ],
+    ["long", new Map([
+        ["co2", 7]
+        ])
+    ]
+]);
+
+function getStartingTotal(surveySettings) {
+    var evaluatorSettings = buildEvaluatorSettings(surveySettings);
+    return calculateActualValues(evaluatorSettings);
+}
+
+function buildEvaluatorSettings(surveySettings) {
+     evaluatorSettings = {
+        // global settings
+        enableDebug: surveySettings.enableDebug,
+        language: surveySettings.language,
+        locale: surveySettings.locale,
+        hiddenTextSelector: surveySettings.hiddenTextSelector,
+
+        diet: {
+            selected: false,
+            enabled: true,
+            visible: true,
+            diet: surveySettings.diet,
+            selectDiet: surveySettings.diet
+        },
+        shortFlights: {
+            selected: false,
+            enabled: true,
+            visible: surveySettings.numShortFlights > 0,
+            numShortFlights: surveySettings.numShortFlights,
+            select: 1}
+    };
+
+    evaluatorSettings.initialMobility = calculateMobility(evaluatorSettings);
+    evaluatorSettings.initialDiet = calculateDiet(evaluatorSettings);
+    return evaluatorSettings;
+}
+
+function calculateActualValues(settings) {
+
+    var actualDiet = calculateDiet(settings);
+    var actualMobility = calculateMobility(settings);
+    var actualTotal = actualDiet + actualMobility;
+    var actual = {
+        actualDiet,
+        actualMobility,
+        actualTotal    
+    };
+    return actual;
+}
+
+function calculateDiet(dietSettings) {
+    if (dietSettings.diet.selected) {
+        var value = dietParameter.get(dietSettings.diet.selectDiet);
+        if (value == null) {
+            console.log("Unknown diet option selected: " + dietSettings.diet);
+            return 0;
+        } else return value;
+    } else {
+        return dietParameter.get(dietSettings.diet.diet);
+    }
+}
+
+function calculateMobility(mobilitySettings) {
+
+    flightsValue = 0;
+    var mobility = 0;
+    if (mobilitySettings.shortFlights.selected) {
+        numShortFlights -= mobilitySettings.shortFlights.select;  
+    } 
+    numShortFlights = mobilitySettings.shortFlights.numShortFlights;
+    flightsValue += numShortFlights * flightParameter.get("short").get("co2");
+    mobility += flightsValue;
+    return mobility;
+}
+
