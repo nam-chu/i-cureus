5e8c6f345b544c725b54a246afe02955dd14e2bb
Hoang-Nam Chu <74799165+nam-chu@users.noreply.github.com>
2025-03-24 23:37:46 -0400

    Update pt_evaluator.js

--- pt_evaluator.js
+++ pt_evaluator.js
@@ -1,215 +1,99 @@
-
-// Start by declaring relevant data structures
-
-// Diet
-const dietParameter = new Map([
-    ["omnivore", 1.837],
-    ["flexitarian", 1.495],
-    ["vegetarian", 1.380],
-    ["vegan", 1.124]
-]);
-
-const flightParameter = new Map([
-    ["short", new Map([
-        ["co2", 0.5]
-        ])
-    ],
-    ["medium", new Map([
-        ["co2", 2]
-        ])
-    ],
-    ["long", new Map([
-        ["co2", 7]
-        ])
-    ]
-]);
-
-const carParameter = new Map([
-    ["petrol-small", new Map([
-        ["co2", 235]
-        ])
-    ],
-    ["petrol-medium", new Map([
-        ["co2", 245]
-        ])
-    ],
-    ["petrol-large", new Map([
-        ["co2", 270]
-        ])
-    ],
-    ["diesel-medium", new Map([
-        ["co2", 245]
-        ])
-    ],
-    ["diesel-large", new Map([
-        ["co2", 285]
-        ])
-    ],
-    ["phev-medium", new Map([
-        ["co2", 180]
-        ])
-    ],
-    ["phev-large", new Map([
-        ["co2", 270]
-        ])
-    ],
-    ["bev-small", new Map([
-       ["co2", 50]
-       ])
-    ],
-    ["bev-medium", new Map([
-       ["co2", 50]
-       ])
-    ],
-    ["bev-large", new Map([
-       ["co2", 55]
-       ])
-   ]
-]);
-
-const ptParameter = new Map([
-    ["train", 0.08 ],
-    ["tram", 0.08 ],
-    ["bus", 0.08 ]
-]);
-
-function getStartingTotal(surveySettings) {
-    var evaluatorSettings = buildEvaluatorSettings(surveySettings);
-    return calculateActualValues(evaluatorSettings);
+function getDiet(val) {
+    // 1-based index (1,2,3,4)
+    let diet = ['', 'omnivore', 'flexitarian', 'vegetarian', 'vegan'];
+    let choice = extractSelectedChoices(val);
+    return diet[choice];
 }
 
-function buildEvaluatorSettings(surveySettings) {
-     evaluatorSettings = {
-        // global settings
-        enableDebug: surveySettings.enableDebug,
-        language: surveySettings.language,
-        locale: surveySettings.locale,
-        hiddenTextSelector: surveySettings.hiddenTextSelector,
-        
-        
-
-        diet: {
-            selected: false,
-            diet: surveySettings.diet,
-            selectDiet: surveySettings.diet
-        },
-        shortFlights: {
-            selected: false,
-            visible: surveySettings.numShortFlights > 0,
-            numShortFlights: surveySettings.numShortFlights,
-            select: 1
-        },
-        mediumFlights: {
-            selected: false,
-            // enabled: true,
-            visible: surveySettings.numMediumFlights > 0,
-            numMediumFlights: surveySettings.numMediumFlights,
-            select: 1,
-        },
-
-        longFlights: {
-            selected: false,
-            // enabled: true,
-            visible: surveySettings.numLongFlights > 0,
-            numLongFlights: surveySettings.numLongFlights,
-            select: 1
-        },
-        reduceKilometrageCar: {
-            selected: false,
-            // enabled: true,
-            visible: surveySettings.hasAccessToCar,
-            carKilometrageYearly: surveySettings.carKilometrageYearly,
-            select: 1.0
-        },
-        compensateKilometrageCarByNone: {
-            // enabled: true,
-            visible: surveySettings.hasAccessToCar,
-            compensatedKilometrageYearly: 0.0,
-            select: 0.0,
-        },
-        replaceCar: {
-            selected: false,
-            // enabled: true,
-            visible: surveySettings.ownsCar,
-            car: surveySettings.car,
-            selectCar: surveySettings.car,
-        },
-        actualCar: surveySettings.car,
-
-        trainKilometrageWeekly: surveySettings.trainKilometrageWeekly,
-        tramKilometrageWeekly: surveySettings.tramKilometrageWeekly,
-        busKilometrageWeekly: surveySettings.busKilometrageWeekly
-    };
-
-    evaluatorSettings.initialMobility = calculateMobility(evaluatorSettings);
-    evaluatorSettings.initialDiet = calculateDiet(evaluatorSettings);
-    return evaluatorSettings;
+function getNbFlights(valNbFlightsTotal, valNbFlights) {
+    let totalChoice = extractSelectedChoices(valNbFlightsTotal); // 0 if no, 1 if yes
+    return (totalChoice != 0) ? parseInt(valNbFlights) : 0;
 }
 
-function calculateActualValues(settings) {
-
-    var actualDiet = calculateDiet(settings);
-    var actualMobility = calculateMobility(settings);
-    var actualTotal = actualDiet + actualMobility;
-    var actual = {
-        actualDiet,
-        actualMobility,
-        actualTotal    
-    };
-    return actual;
+function getOwnsCar(val) {
+    // string with selected values (1,2,3,4,5,6)
+    let choices = extractSelectedChoices(val, true); // extract the selected choices from the string (as array)
+    return choices[0] == 1;
 }
 
-function calculateDiet(dietSettings) {
-    if (dietSettings.diet.selected) {
-        var value = dietParameter.get(dietSettings.diet.selectDiet);
-        if (value == null) {
-            console.log("Unknown diet option selected: " + dietSettings.diet);
-            return 0;
-        } else return value;
+function getCarAccess(val) {
+    // string with selected values (1,2)
+    return extractSelectedChoices(val) == 1;
+}
+
+function getFuelType(val) {
+    // 1-based index (1,2,3,4)
+    let fuels = ['', 'petrol', 'diesel', 'bev', 'phev'];
+    let choice = extractSelectedChoices(val);
+    return fuels[choice];
+}
+
+function getCarSize(val) {
+    // 1-based index (1,2,3)
+    let size = ['', 'small', 'medium', 'large'];
+    let choice = extractSelectedChoices(val);
+    return size[choice];
+}
+
+function getCar(valFuel, valSize) {
+    let fuelType = getFuelType(valFuel);
+    let carSize = getCarSize(valSize);
+    getFuelType(valFuel) + "-" + getCarSize(valSize);
+    if(fuelType == '' && carSize == '') {
+        return "";
+    } 
+    return fuelType + "-" + carSize;
+}
+
+function getPtKm(valUsesPt, valPtKm) {
+    let useChoice = extractSelectedChoices(valUsesPt);
+    return (useChoice != 0) ? parseInt(valPtKm) : 0; 
+}
+
+function extractSelectedChoices(answer, forceReturnArray = false) {
+    let NO_ANSWER = 0;
+    var indices = [];
+    if (answer == "") {
+        indices.push(NO_ANSWER);
     } else {
-        return dietParameter.get(dietSettings.diet.diet);
+        while(answer.includes(",")) {
+            // read number
+            indices.push(parseInt(answer.charAt(0)));
+            // delete the read number and ", "
+            answer = answer.substring(3);
+        }
+        // last one
+        indices.push(parseInt(answer.charAt(0)));
+    }
+
+    if (indices.length == 1 && !forceReturnArray) {
+        return indices[0];
+    } else {
+        return indices;
     }
 }
 
-function calculateMobility(mobilitySettings) {
-    let mobility = 0;
-    let flightsValue = 0;
-
-    let numShortFlights = mobilitySettings.shortFlights.numShortFlights;
-    let numMediumFlights = mobilitySettings.mediumFlights.numMediumFlights;
-    let numLongFlights = mobilitySettings.longFlights.numLongFlights;
-
-    if (mobilitySettings.shortFlights.selected) {
-        numShortFlights -= mobilitySettings.shortFlights.select;
-    }
-    if (mobilitySettings.mediumFlights.selected) {
-        numMediumFlights -= mobilitySettings.mediumFlights.select;
-    }
-    if (mobilitySettings.longFlights.selected) {
-        numLongFlights -= mobilitySettings.longFlights.select;
-    }
-
-    flightsValue += numShortFlights * flightParameter.get("short").get("co2");
-    flightsValue += numMediumFlights * flightParameter.get("medium").get("co2");
-    flightsValue += numLongFlights * flightParameter.get("long").get("co2");
-    mobility += flightsValue;
-
-    if (mobilitySettings.replaceCar && mobilitySettings.replaceCar.car != "") {
-        let carInfo = carParameter.get(mobilitySettings.replaceCar.car);
-        let carKilometrage = mobilitySettings.reduceKilometrageCar.carKilometrageYearly;
-        let carCo2 = carInfo.get("co2");
-        // Assume carCo2 is in grams per km; convert to tons per year:
-        let carEmissions = carCo2 * carKilometrage / 1_000_000;
-        mobility += carEmissions;
-    }
-
-    ptValue = 0;
-    ptValue += ptParameter.get("train") * mobilitySettings.trainKilometrageWeekly * 52 / 1000;
-    ptValue += ptParameter.get("tram") * mobilitySettings.tramKilometrageWeekly * 52 / 1000;
-    ptValue += ptParameter.get("bus") * mobilitySettings.busKilometrageWeekly * 52 / 1000;
-    mobility += ptValue;
-
-    return mobility;
+function parseIntOrZero(answer) {
+    let parsed = parseInt(answer);
+    return isNaN(parsed) ? 0 : parsed;
 }
 
+function formatSurveySettings(data) {
+
+    var surveySettings = {
+        diet: getDiet(data.q1),
+        numShortFlights: getNbFlights(data.q2, data.q3),
+        numMediumFlights: getNbFlights(data.q2, data.q4),
+        numLongFlights: getNbFlights(data.q2, data.q5),
+        hasAccessToCar: getCarAccess(data.q6),
+        ownsCar: getOwnsCar(data.q7),
+        carKilometrageYearly: parseIntOrZero(data.q10),
+        car: getCar(data.q8, data.q9),
+        trainKilometrageWeekly: getPtKm(data.q11, data.q12),
+        tramKilometrageWeekly: 0,
+        busKilometrageWeekly: 0,
+    };
+
+    return surveySettings;
+}
 
